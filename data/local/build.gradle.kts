ce58df1022fe76f61b1c645d4d136d099ab25b21
welton-carvalho <welton.apcarvalho@gmail.com>
2026-05-10 20:57:39 -0300

    adjust build

--- data/local/build.gradle.kts
+++ data/local/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.pokedexlab.android.library)
     alias(libs.plugins.objectbox)
-    alias(libs.plugins.ksp)
 }
 
 android {
@@ -12,6 +11,5 @@ dependencies {
     implementation(project(":core:model"))
     implementation(libs.objectbox.android)
     implementation(libs.objectbox.kotlin)
-    ksp(libs.objectbox.processor)
     implementation(libs.koin.android)
 }
